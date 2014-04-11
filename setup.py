271ae5172c988727ef7e9117bea59b1539ef99a0
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-11 17:52:43 +0200

    Added first version of setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import os
+
+backbone_calendar = __import__('backbone_calendar')
+
+
+
+setup(
+    name='django-backbone-calendar',
+    packages=find_packages(),
+    author='Gabriel Pichot',
+    author_email='gabriel.pichot@gmail.com',
+    url='https://github.com/gpichot/django-backbone-calendar',
+    description=(
+        'Django Backbone Calendar is an app for '
+        'Django which aims to manipulate easily a'
+        'calendar.'
+    ),
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python',
+    ],
+    keywords=['calendar', 'backbone', 'event', 'agenda'],
+)
