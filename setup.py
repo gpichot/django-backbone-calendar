1e904533a122cba34f1623221404cf3be3ce9d89
Gabriel Pichot <gabriel.pichot@gmail.com>
2014-04-11 18:09:10 +0200

    Erratum : Added django-bootstrap-form to requirements (setup.py)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
        'Django >= 1.5',
        'South >= 0.8.4',
        'django-backbone >= 0.3.1',
-       'django-bootstram-form >= 3.1',
+       'django-bootstrap-form >= 3.1',
        'django-ical >= 1.2',
     ],
 )
